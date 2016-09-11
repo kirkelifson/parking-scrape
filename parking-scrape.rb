9d00ee59176a71b92e21b32c5492493bc8aee25d
Kirk Elifson <kirk@parodybit.net>
2016-09-11 11:53:07 -0400

    move sql to block

--- parking-scrape.rb
+++ parking-scrape.rb
@@ -2,16 +2,12 @@ require 'sqlite3'
 require 'nokogiri'
 require 'open-uri'
 
-db = SQLite3::Database.new("parking.db")
-
-#db.execute <<SQL
 #create table parking (
 #id integer primary key,
 #name varchar(40),
 #available integer,
 #total integer
 #)
-#SQL
 
 page = Nokogiri::HTML(open("http://secure.parking.ucf.edu/GarageCount/"))
 
@@ -26,9 +22,9 @@ page.css('table > tr > td.dxgv').text.split(/[\d]+\/(.*)/).compact.each_slice(2)
 total_spaces.compact!.map!(&:chomp).map!(&:to_i)
 
 puts "Name\t\tAvailable\tTotal"
-garage_names.zip(spaces_available, total_spaces).each do |name, avail, total|
-  puts "#{name}\t#{avail}\t\t#{total}"
-  db.execute <<SQL
-  insert into parking values("#{name}", #{avail}, #{total}, datetime());
-SQL
+SQLite3::Database.new("/var/www/parking-scrape/parking.db") do |db|
+  garage_names.zip(spaces_available, total_spaces).each do |name, avail, total|
+    puts "#{name}\t#{avail}\t\t#{total}"
+    db.execute("insert into parking values('#{name}', #{avail}, #{total}, datetime())")
+  end
 end
